e9a71f41335e4829af9c7edfd4b0bfb21c0ee7fb
Louise <louiselai88@gmail.com>
2014-05-06 15:53:51 +0100

    step one of scenario one passed! yay!

--- features/step_definitions/calculator_steps.rb
+++ features/step_definitions/calculator_steps.rb
@@ -1,5 +1,5 @@
-Given(/^the input "(.*?)"$/) do |arg1|
-	pending # express the regexp above with the code you wish you had
+Given(/^the input "(.*?)"$/) do |input|
+	@input = input
 end
 
 When(/^the calculator is run$/) do
