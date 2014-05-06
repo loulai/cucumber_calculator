503fc4923a9aef9e4c3f3937502d0c1ba26496ea
Louise <louiselai88@gmail.com>
2014-05-06 17:34:10 +0100

    all three steps pass; now refactoring so it is a legitimate pass

--- features/step_definitions/calculator_steps.rb
+++ features/step_definitions/calculator_steps.rb
@@ -7,6 +7,6 @@ When(/^the calculator is run$/) do
 	raise('command failed!') unless $?.success?
 end
 
-Then(/^the output should be "(.*?)"$/) do |arg1|
-	pending # express the regexp above with the code you wish you had
+Then(/^the output should be "(.*?)"$/) do |expected_output|
+	expect(@output).to eq expected_output
 end
